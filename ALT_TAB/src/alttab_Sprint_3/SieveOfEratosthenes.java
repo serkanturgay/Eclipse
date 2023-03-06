52bb8ed1bf302691b92d4fbfe5d45c90467d28c6
serkanturgay <77546669+serkanturgay@users.noreply.github.com>
2023-03-06 18:52:12 +0300

    06.03.2023

--- ALT_TAB/src/alttab_Sprint_3/SieveOfEratosthenes.java
+++ ALT_TAB/src/alttab_Sprint_3/SieveOfEratosthenes.java
@@ -7,39 +7,34 @@ import java.util.Scanner;
 public class SieveOfEratosthenes {
 
 	public static void main(String[] args) {
-		int tempNum = 2;
-		int a = 0;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the number : ");
 		int arrayLength = scan.nextInt();
-		int[] arrayNumber = new int[arrayLength-1];
-		int[] tempArray = new int[arrayLength-1];
-		for (int i = 0; i < arrayNumber.length; i++) {
-			
-			if (tempNum != arrayLength+1) {
-				arrayNumber[i] = tempNum;
-				tempNum++;
-			}
-			
-		}
-		
-		System.out.println(Arrays.toString(arrayNumber));
-		
-		for (int i = 2; i < tempArray.length; i++) {
-			for (int j = 0; j < arrayNumber.length; j++) {
-				if (arrayNumber[j] % i == 0) {
-					tempArray[a] = arrayNumber[j];
-
+		boolean[] arrayNumber = new boolean[arrayLength];
+		Arrays.fill(arrayNumber, true);
+		for (int i = 2; i < arrayLength; i++) {
+			for (int j  = 2; i * j < arrayLength; j ++) {
+				if (arrayNumber[i * j] == true) {
+					arrayNumber[i * j] = false;
 				}
 			}
-			if (j == arrayNumber.length) {
-				break;
-			}
-			
 		}
+		arrayNumber[0] = false;
+		arrayNumber[1] = false;	
+		int primeNumber = 0;
+		for (boolean b : arrayNumber) {
+			if (b) {
+				primeNumber++;
+			}
+		}
+		System.out.println("There are " + primeNumber + " Prime Numbers  ");
+		System.out.print("prime numbers from 1 to " + arrayLength + " : ");
 		
-		System.out.println(Arrays.toString(tempArray));
-
+		for (int i = 0; i < arrayNumber.length; i++) {
+			if (arrayNumber[i] == true) {
+				System.out.print(i + ", ");
+			}
+		}
 	}
 
 }
